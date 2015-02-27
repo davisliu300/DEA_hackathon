9a4c39da2315ae62b5b6f5b300cf1f6a09ae3019
davisliu300 <davis.liu300@gmail.com>
2015-02-26 17:06:08 -0800

    javascript for JSON file

--- js/dea.js
+++ js/dea.js
@@ -1,36 +1,29 @@
 // DEA javascript file
 
 var team = "DEA";
+	
+for(var index = 0; index < movies.feed.entry.length; index++){	
 
+	var moviesTitles = movies.feed.entry[index].title.label;
+	var asideTitleDiv = $('<div/>').attr('id', "movie" + index).html(moviesTitles).attr('index',index);
+	$('aside').append(asideTitleDiv);
+	
+// looping thro protoMovieClasses and apply it to Movie title (color)	
+	var protoMovieClasses = ['protoMovieTitleRed', 'protoMovieTitleBlack','protoMovieTitleGreen'];
+	asideTitleDiv.addClass(protoMovieClasses[index]);
+	
+	
+	$('#movie' + index).click(function(){
+//		var this_moviesSummary = moviesSummary;
+var moviesList = movies.feed.entry[$(this).attr('index')];
+//		$('section').html(movies.feed.entry[$(this).attr('index')].summary.label);	
 
+$('section').html(
 
-console.log(movies.feed.entry[0].summary);
-
-var firstTenObj = movies.feed;
-for(var i = 0; i < firstTenObj.entry.length; i++){
-	console.log(firstTenObj.entry[i]);
+"Genre: "+ moviesList.category.attributes.label + "<br>" +
+"Release Date: "+ moviesList["im:releaseDate"].attributes.label+ "<br/>"+ 
+"Summary: " + moviesList.summary.label);		
+});
 }
 
 
-
-
-
-/*
-
-$.getJSON( "movies.json", function( data ) {
-var items = [];
-$.each( data, function( key, val ) {
-items.push( "<li id='" + key + "'>" + val + "</li>" );
-});
-
-$( "<ul/>", {
-"class": "my-new-list",
-html: items.join( "" )
-}).appendTo( "body" );
-});
-
-----------------------------
-
-
-
-*/
