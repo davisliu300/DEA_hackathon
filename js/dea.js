2f6db67a7510d228b24b4afdaaed8145f065d87e
davisliu300 <davis.liu300@gmail.com>
2015-02-26 18:53:32 -0800

    final javascript

--- js/dea.js
+++ js/dea.js
@@ -1,7 +1,9 @@
-// DEA javascript file
+//DEA javascript file
 
 var team = "DEA";
-	
+
+
+
 for(var index = 0; index < movies.feed.entry.length; index++){	
 
 	var moviesTitles = movies.feed.entry[index].title.label;
@@ -9,20 +11,21 @@ for(var index = 0; index < movies.feed.entry.length; index++){
 	$('aside').append(asideTitleDiv);
 	
 // looping thro protoMovieClasses and apply it to Movie title (color)	
+/*
 	var protoMovieClasses = ['protoMovieTitleRed', 'protoMovieTitleBlack','protoMovieTitleGreen'];
 	asideTitleDiv.addClass(protoMovieClasses[index]);
-	
+	*/
 	
 	$('#movie' + index).click(function(){
-//		var this_moviesSummary = moviesSummary;
-var moviesList = movies.feed.entry[$(this).attr('index')];
-//		$('section').html(movies.feed.entry[$(this).attr('index')].summary.label);	
-
-$('section').html(
-
+		var moviesList = movies.feed.entry[$(this).attr('index')];
+		$('section').html(
 "Genre: "+ moviesList.category.attributes.label + "<br>" +
 "Release Date: "+ moviesList["im:releaseDate"].attributes.label+ "<br/>"+ 
-"Summary: " + moviesList.summary.label);		
+"Summary: " + moviesList.summary.label);
+console.log(moviesList['im:image'][0].label);
+$('section').append("<img src ='"+ moviesList['im:image'][0].label+"'>");	
 
+});
+}
 
 
